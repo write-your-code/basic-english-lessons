f4d8a18f7ed1fca4c23f3fb8f048d4ea0db29234
write-your-code <codingtest1986@gmail.com>
2024-10-09 10:26:39 +0500

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
+
+// solution to deploy gh-pages using vite
+// https://github.com/tschaub/gh-pages/issues/485
